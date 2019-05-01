b3f748fb43fdc4cbec375b879c7648ab2d7b06df
alex <picusalex@yahoo.fr>
2019-05-01 12:00:35 +0200

    webserver v1.0

---
+++ autopial-webserver.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+
+import sys
+import uuid
+
+from flask import Flask, render_template, Response, jsonify, send_file, abort
+import logging
+import paho.mqtt.client as mqtt #import the client1
+from flask_socketio import SocketIO
+import redis
+
+logger = logging.getLogger("sibus-server")
+logger.setLevel(logging.DEBUG)
+steam_handler = logging.StreamHandler()
+stream_formatter = logging.Formatter('%(asctime)s|%(levelname)08s | %(message)s')
+steam_handler.setFormatter(stream_formatter)
+logger.addHandler(steam_handler)
+
+redis_conn = None
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    return render_template('index.html')
+
+@app.route("/camera_live.html")
+def camera_live_html():
+    return render_template('camera_live.html')
+
+@app.route("/camera_timelap.html")
+def camera_timelap_html():
+    return render_template('camera_timelap.html')
+
+@app.route("/timelaps.html")
+def timelaps_page():
+    return render_template('timelaps.html')
+
+@app.route("/system_status.html")
+def system_status_html():
+    return render_template('system_status.html')
+
+@app.route("/gps_info.html")
+def gps_info_html():
+    return render_template('gps_info.html')
+
+def emit_all():
+    logger.info("Emitting all topics on SocketIO")
+    for key in redis_conn.scan_iter():
+        payload_str = redis_conn.get(key)
+        payload = json.loads(payload_str)
+        logger.info(" + {} : {}".format(key, payload_str))
+        socketio.emit(key, payload)
+
+@socketio.on('connect')
+def client_connected():
+    logger.debug("New client connected on SocketIO")
+    emit_all()
+
+def on_message(client, userdata, message):
+    payload_str = message.payload.decode("utf-8")
+    redis_conn.set(message.topic, payload_str)
+
+    logger.info("Forward MQTT message to SocketIO: {} = {}".format(message.topic, payload_str))
+    payload = json.loads(payload_str)
+    socketio.emit(message.topic, payload)
+
+if __name__ == '__main__':
+    try:
+        redis_conn = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+    except redis.exceptions.ConnectionError as e:
+        logger.error("Connection to redis refused. Redis installed ? (sudo apt install redis-server)")
+        sys.exit(1)
+
+    broker_address = "localhost"
+    mqtt_client = mqtt.Client("sibus-server-{}".format(uuid.uuid4().hex))
+    mqtt_client.on_message = on_message
+    mqtt_client.connect(broker_address)
+    mqtt_client.loop_start()
+    mqtt_client.subscribe("autopial/#", qos=1)
+
+    socketio.run(app, host="0.0.0.0")
+
+    mqtt_client.loop_stop()
---
+++ static/js/utils.js
@@ -0,0 +1,120 @@
+
+function pretty_timeDifference(strtdatetime) {
+  var datetime = new Date(strtdatetime);
+  var oResult = {};
+
+  var oToday = new Date();
+
+  var nDiff = oToday.getTime() - datetime.getTime();
+
+  // Get diff in days
+  oResult.days = Math.floor(nDiff / 1000 / 60 / 60 / 24);
+  nDiff -= oResult.days * 1000 * 60 * 60 * 24;
+
+  // Get diff in hours
+  oResult.hours = Math.floor(nDiff / 1000 / 60 / 60);
+  nDiff -= oResult.hours * 1000 * 60 * 60;
+
+  // Get diff in minutes
+  oResult.minutes = Math.floor(nDiff / 1000 / 60);
+  nDiff -= oResult.minutes * 1000 * 60;
+
+  // Get diff in seconds
+  oResult.seconds = Math.floor(nDiff / 1000);
+
+  // Render the diffs into friendly duration string
+
+  // Days
+  var sDays = '00';
+  if (oResult.days > 0) {
+      sDays = String(oResult.days);
+  }
+  if (sDays.length === 1) {
+      sDays = '0' + sDays;
+  }
+
+  // Format Hours
+  var sHour = '00';
+  if (oResult.hours > 0) {
+      sHour = String(oResult.hours);
+  }
+  if (sHour.length === 1) {
+      sHour = '0' + sHour;
+  }
+
+  //  Format Minutes
+  var sMins = '00';
+  if (oResult.minutes > 0) {
+      sMins = String(oResult.minutes);
+  }
+  if (sMins.length === 1) {
+      sMins = '0' + sMins;
+  }
+
+  //  Format Seconds
+  var sSecs = '00';
+  if (oResult.seconds > 0) {
+      sSecs = String(oResult.seconds);
+  }
+  if (sSecs.length === 1) {
+      sSecs = '0' + sSecs;
+  }
+
+  //  Set Duration
+  var sDuration = sDays + ':' + sHour + ':' + sMins + ':' + sSecs;
+  oResult.duration = sDuration;
+
+  // Set friendly text for printing
+  if(oResult.days === 0) {
+
+      if(oResult.hours === 0) {
+
+          if(oResult.minutes === 0) {
+              var sSecHolder = oResult.seconds > 1 ? 'Seconds' : 'Second';
+              oResult.friendlyNiceText = oResult.seconds + ' ' + sSecHolder;
+          } else {
+              var sMinutesHolder = oResult.minutes > 1 ? 'Minutes' : 'Minute';
+              oResult.friendlyNiceText = oResult.minutes + ' ' + sMinutesHolder;
+          }
+
+      } else {
+          var sHourHolder = oResult.hours > 1 ? 'Hours' : 'Hour';
+          var sMinutesHolder = oResult.minutes > 1 ? 'Minutes' : 'Minute';
+          oResult.friendlyNiceText = oResult.hours + ' ' + sHourHolder + ' ' + oResult.minutes + ' ' + sMinutesHolder;
+      }
+  } else {
+      var sDayHolder = oResult.days > 1 ? 'Days' : 'Day';
+      var sHourHolder = oResult.hours > 1 ? 'Hours' : 'Hour';
+      var sMinutesHolder = oResult.minutes > 1 ? 'Minutes' : 'Minute';
+      oResult.friendlyNiceText = oResult.days + ' ' + sDayHolder + ' ' +oResult.hours + ' ' + sHourHolder + ' ' + oResult.minutes + ' ' + sMinutesHolder;
+  }
+
+  return oResult;
+}
+
+function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+function formatBps(bps, decimals = 2) {
+    if (bps === 0) return '0 kbps';
+
+    bps = bps / 1024.0
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['kbps', 'Mbps', 'Gbps'];
+
+    const i = Math.floor(Math.log(bps) / Math.log(k));
+
+    if (bps < 1) return bps.toFixed(dm)+' kbps'
+    return parseFloat((bps / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
